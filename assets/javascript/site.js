4bbb26ddd5a6d3f8a60642ff00b82da54806fe9d
Megha Prasad Bindiganavale <Megha@Meghas-MacBook-Pro.local>
2019-06-05 22:08:14 -0700

    create renderButton function

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -4,10 +4,27 @@ var emotions = ["happy", "sad", "angry"];
 // create div to dump all the gifs
 $("#buttonsDiv").append('<div class="json"></div>');
 
+function displayGifs(){
+    $(".json").empty();
+    
+}
+function renderButton(){
+    $("#buttons-view").empty();//clear all the buttons
+    //loop through array and dynamically add button for each
+    for (var i=0;i<emotions.length;i++){
+        var newButton = $("<button>"); //create button
+        newButton.addClass("emotion"); //add class 
+        newButton.attr("data-name", emotions[i]); //add data value
+        newButton.text(emotions[i]); //name the button
+        $("#buttons-view").append(newButton);
+    }
+}
 //get userInput and push that to the emotions array 
 $("#add-movie").on("click", function(event) {
     event.preventDefault();
     var emotion = $("#add-emotion").val().trim();//get value
     emotions.push(emotion);
-// function that displays gif
+    //call function that rendersButton to the dom
+    renderButton(); 
 });
+
